ed341200f94801f5484113b57b8eebc450becc59
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-09-08 06:26:40 +0300

    idk

--- PID.py
+++ PID.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 class PID:
     def __init__(self,Kp,Ki,Kd,Kaw,T_c,T,max,min,max_rate):
@@ -38,7 +40,7 @@ class PID:
         #sat command
         if self.command>self.max:
             self.command = self.max
-        elif self.command<sat.min:
+        elif self.command<self.min:
             self.command = self.min
         else:
             self.command_sat=self.command
@@ -49,17 +51,61 @@ class PID:
             self.command_sat=self.command_sat_prev-self.max_rate*self.T
         #store prev sat command
         self.command_sat_prev=self.command_sat
-        def set_target(self,setpoint):
-            # set target value
-            self.setpoint  = setpoint
-        def printTel(self):
-            print("Kp: ",self.Kp)
-            print("Ki: ",self.Ki)
-            print("Kd: ",self.Kd)
-            print("Kaw: ",self.Kaw)
-            print("T_c: ",self.T_c)
-            print("T: ",self.T)
-            print("max: ",self.max)
-            print("min: ",self.min)
-            print("max_rate: ",self.max_rate)
-            
+        return self.command_sat
+
+    def set_target(self,setpoint):
+        # set target value
+        self.setpoint=setpoint
+
+    def printTel(self):
+        print("Kp: ",self.Kp)
+        print("Ki: ",self.Ki)
+        print("Kd: ",self.Kd)
+        print("Kaw: ",self.Kaw)
+        print("T_c: ",self.T_c)
+        print("T: ",self.T)
+        print("max: ",self.max)
+        print("min: ",self.min)
+        print("max_rate: ",self.max_rate)
+def main():
+            time = 0.1
+            end_time =25
+            length  = round(end_time/time)
+            t = np.zeros(length)
+            stp = np.zeros(length)
+            v=np.zeros(length)
+            command = np.zeros(length)
+            Kp = 800.0
+            Ki = 70.0
+            Kaw = 1.0
+            Kd = 20.0
+            T_C = 0.1
+            pid = PID(Kp,Ki,Kd,Kaw,T_C,time,22000,0,30000)
+            for idx in range(0,length):
+                t[idx] = idx*time
+                if t[idx]>5:
+                    stp[idx]=42
+                command[idx] =pid.compute(v[idx],stp[idx])
+                if idx<length-1:
+                    v[idx+1] = v[idx]+command[idx]*time
+            pid.printTel()
+            print("final speed:" , v[length - 1])
+
+            plt.subplot(2, 1, 1)
+            plt.plot(t, v, label="Response")
+            plt.plot(t, stp, '--', label="Setpoint")
+            plt.xlabel("Time [s]")
+            plt.ylabel("Speed [m/s]")
+            plt.legend()
+            plt.grid()
+
+            plt.subplot(2, 1, 2)
+            plt.plot(t, command, label="Command")
+            plt.xlabel("Time [s]")
+            plt.ylabel("Force [N]")
+            plt.legend()
+            plt.grid()
+
+            plt.savefig("response.png")
+
+main()
