06e5c89353ca64c7b6dbcb9cbcc2c0ae574cf440
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-09-08 05:54:50 +0300

    sa7?

--- PID.py
+++ PID.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 class PID:
     def __init__(self,Kp,Ki,Kd,Kaw,T_c,T,max,min,max_rate):
         self.Kp = Kp# proportional gain
@@ -16,7 +18,7 @@ class PID:
         self.command_prev = 0 #previous command
         self.command_sat = 0 # current saturated
         self.command = 0 # current command
-    def Step(self,measurement,setpoint):
+    def compute(self,measurement,setpoint):
         """ Steps to excute pid controller
         Inputs: 
         measurement: current measuremnt of process
@@ -47,3 +49,17 @@ class PID:
             self.command_sat=self.command_sat_prev-self.max_rate*self.T
         #store prev sat command
         self.command_sat_prev=self.command_sat
+        def set_target(self,setpoint):
+            # set target value
+            self.setpoint  = setpoint
+        def printTel(self):
+            print("Kp: ",self.Kp)
+            print("Ki: ",self.Ki)
+            print("Kd: ",self.Kd)
+            print("Kaw: ",self.Kaw)
+            print("T_c: ",self.T_c)
+            print("T: ",self.T)
+            print("max: ",self.max)
+            print("min: ",self.min)
+            print("max_rate: ",self.max_rate)
+            
